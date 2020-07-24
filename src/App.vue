e9dea8047b20ae719bebc05d59a76a72c6c96a0f
Rubin <rubyyy2k@gmail.com>
2020-07-24 16:06:26 +0200

    Update App.vue
    Added commentry on methods as well as searchbox and temperature method

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/openweather-logo.png">
+  <div id="app" :class="typeof weather.main">
+    <main>
+<!-- Default search box, on keypress fetchWeather() method is called -->
+      <div class="search-box">
+        <input 
+          type="text" 
+          class="search-bar" 
+          placeholder="Search..."
+          v-model="query"
+          @keypress="fetchWeather"
+        />
+      </div>
+
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <div class="location-box">
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date">{{ dateBuilder() }}</div>
+        </div>
+
+        <div class="weather-box">
+          <!-- Returns Temperature to nearest integer -->
+          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
+        </div>
+      </div>
+    </main>
   </div>
-  
 </template>
 
 <script>
@@ -12,21 +35,50 @@ export default {
   data(){
     return{
       //OpenWeather API Key
-      api_key: '4a36aa61c8a3561cbab410e2cf241d2e'
+      api_key: '4a36aa61c8a3561cbab410e2cf241d2e',
+      query: '',
+      weather: {}
+    }
+  },
+  methods: {
+    fetchWeather (e) {
+      //API Query performed on enter key press
+      if (e.key == "Enter") {
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+      }
+    },
+    setResults (results) {
+      this.weather = results;
+    },
+    dateBuilder () {
+      //Date Generated through array of months/days and calling on built-in js date library - returning day date month year
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+      return `${day} ${date} ${month} ${year}`;
     }
   }
 }
 </script>
 
 <style>
+
 /* Component positioning and Styling */
+
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
 
-body{
+body {
   font-family: 'montserrat', sans-serif;
 }
 
@@ -102,6 +154,8 @@ main {
   font-style: italic;
   text-align: center;
 }
+
+/* Weather box dimensions and styling*/
 
 .weather-box {
   text-align: center;
